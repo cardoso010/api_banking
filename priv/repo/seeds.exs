012ca07e1e05aae9408ba997d184a0953ab99795
Gabriel Cardoso Luiz <gabriel.cardoso010@gmail.com>
2021-01-20 10:10:18 -0300

    user's seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,23 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+{:ok, admin} =
+  ApiBanking.Users.Mutator.create(%{
+    name: "Admin",
+    email: "admin@admin.com",
+    password: "admin"
+  })
+
+admin
+|> ApiBanking.Accounts.Mutator.create_with_assoc()
+
+{:ok, user1} =
+  ApiBanking.Users.Mutator.create(%{
+    name: "User1",
+    email: "user1@user1.com",
+    password: "user1"
+  })
+
+user1
+|> ApiBanking.Accounts.Mutator.create_with_assoc()
