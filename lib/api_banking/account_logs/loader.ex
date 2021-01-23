ffe3e97fad0ff0edf60f38c747bf585f67305a17
Gabriel Cardoso Luiz <gabriel.cardoso010@gmail.com>
2021-01-23 17:09:26 -0300

    new func to get report

--- lib/api_banking/account_logs/loader.ex
+++ lib/api_banking/account_logs/loader.ex
@@ -1,5 +1,57 @@
 defmodule ApiBanking.AccountLogs.Loader do
+  import Ecto.Query
   alias ApiBanking.{AccountLog, Loaders.Commands, Repo}
+
+  @doc """
+  Get total sum of (transactions) AccountLog between dates
+
+  ## Examples
+
+      iex> get_total_between_dates(~N[2021-02-15 14:38:32], ~N[2021-02-20 00:38:32])
+      200
+
+  """
+  def get_total_between_dates(start_date, end_date) do
+    [total] =
+      from(p in AccountLog,
+        where: fragment("inserted_at BETWEEN ? AND ?", ^start_date, ^end_date),
+        select: sum(p.amount)
+      )
+      |> Repo.all()
+
+    total
+  end
+
+  @doc """
+  Get total sum of (transactions) AccountLog by day
+
+  ## Examples
+
+      iex> get_total_by_day("2021-02-15")
+      200
+
+  """
+  def get_total_by_day(date) do
+    [total] =
+      from(a in AccountLog,
+        where: fragment("date_trunc('day', ?)", a.inserted_at) == type(^date, :date),
+        select: sum(a.amount)
+      )
+      |> Repo.all()
+
+    total
+  end
+
+  @doc """
+  Get total sum of (transactions) AccountLog
+  """
+  def get_total_sum do
+    [total] =
+      from(a in AccountLog, select: sum(a.amount))
+      |> Repo.all()
+
+    total
+  end
 
   @doc """
   Gets a single account_log.
--- test/api_banking/account_logs/loaders_test.exs
+++ test/api_banking/account_logs/loaders_test.exs
@@ -27,4 +27,28 @@ defmodule ApiBanking.AccountLogs.LoadersTest do
       assert Loader.all_by(%{origin_id: account.id}) == [account_log]
     end
   end
+
+  describe "get_total_sum" do
+    test "return total of transactions", %{account: account} do
+      insert(:account_log, origin_id: account.id)
+      assert Loader.get_total_sum() == 1000
+    end
+  end
+
+  describe "get_total_by_day" do
+    test "return transactions's total of today", %{account: account} do
+      insert(:account_log, origin_id: account.id)
+      today = Timex.now() |> Timex.format!("{YYYY}-{0M}-{D}")
+      assert Loader.get_total_by_day(today) == 1000
+    end
+  end
+
+  describe "get_total_between_dates" do
+    test "return transactions's total of between dates", %{account: account} do
+      insert(:account_log, origin_id: account.id)
+      start_date = Timex.beginning_of_month(Timex.now())
+      end_date = Timex.end_of_month(Timex.now())
+      assert Loader.get_total_between_dates(start_date, end_date) == 1000
+    end
+  end
 end
